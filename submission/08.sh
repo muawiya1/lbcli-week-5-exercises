2fce3a7cc2ca82cf955008ec69365c623305a37d
muawiya1 <muawiyaabk@gmail.com>
2026-04-14 15:28:14 +0100

    Fix 01

--- submission/08.sh
+++ submission/08.sh
@@ -8,7 +8,7 @@ publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 #seconds=$((6*30*24*60*60)) - 15552000 seconds
 #nvalue=$(($seconds/512)) - 30375
 
-#convert to hex
+#convert to hex value
 #hexvalue=$(printf '%x\n' $nvalue)
 
 #set the 23rd bit as 1 to make bitcoin interprete it as time instead of block.
@@ -17,6 +17,7 @@ publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 #time value is 4224679
 #convert publicKey to publicKeyHas
 # echo -n $publicKey | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160
+
 publicKeyHash=1e51fcdc14be9a148bb0aaec9197eb47c83776fb
 
 CSV_Script_TimeBased=$(btcc 4224679 OP_CHECKSEQUENCEVERIFY OP_DROP OP_DUP OP_HASH160 $publicKeyHash OP_EQUALVERIFY OP_CHECKSIG)
